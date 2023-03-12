4d2e924ec5414a78d7cc40327f6363855be218bb
Gaurav Singh <sgaurav4683@gmail.com>
2023-03-12 22:22:36 +0530

    navbar and other pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Shop from "./pages/Shop";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
